20b868fce140f0c52b5952a2ea62206679f7eaf1
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-12-11 08:02:26 -0500

    Fix formula class name

--- Formula/uli.rb
+++ Formula/uli.rb
@@ -1,4 +1,4 @@
-class ULI < Formula
+class Uli < Formula
   desc "Universal Loan Identifier (ULI) validation and check digit generation"
   url "https://github.com/jmarin/uli/archive/v0.2.0.tar.gz"
   sha256 "51bd372617fdc159639181b508e04fd7aaea244e"
